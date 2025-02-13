d64a53aec5c2496a86aa17be3bda33e117ea426d
Tan Sean Leng <seanleng99@gmail.com>
2025-02-14 00:51:19 +0800

    Update AWS endpoint

--- backend/product/src/main/java/com/spring/constants/WebConstants.java
+++ backend/product/src/main/java/com/spring/constants/WebConstants.java
@@ -2,13 +2,15 @@ package com.spring.constants;
 
 public class WebConstants {
 
+	// Parameters
 	public static final String USER_EMAIL = "email";
 	public static final String USER_PASSWORD = "password";
 	public static final String USER_ADMIN_ROLE = "admin";
 	public static final String USER_CUST_ROLE = "customer";
 
-	// Parameters
 	public static final String USER_AUTH_TOKEN = "AUTH_TOKEN";
+
+	// Product
 	public static final String PROD_FILE = "file";
 	public static final String PROD_DESC = "description";
 	public static final String PROD_NAME = "productname";
@@ -16,6 +18,7 @@ public class WebConstants {
 	public static final String PROD_QUANITY = "quantity";
 	public static final String PROD_ID = "productId";
 
+	// Order
 	public static final String ORD_STATUS = "orderStatus";
 	public static final String ORD_ID = "orderId";
 
@@ -27,12 +30,12 @@ public class WebConstants {
 	public static final String ADR_ZP = "zipcode";
 	public static final String PHONE = "phonenumber";
 
-	// BUFCART
+	// Bufcart
 	public static final String BUF_ID = "bufcartid";
 	public static final String BUF_QUANTITY = "quantity";
 
-	// Domains
+	// Domain
 	public static final String ALLOWED_URL = "http://localhost:4200";
-	public static final String ALLOWED_URL_PROD = "http://nus-iss-grp1-frontend.s3-website-ap-southeast-1.amazonaws.com";
+	public static final String ALLOWED_URL_PROD = "http://nus-iss-grp-project-gcsus-frontend.s3-website-ap-southeast-1.amazonaws.com";
 
 }
--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,9 +1,9 @@
 export const environment = {
   production: true,
   // user base url
-  userBaseUrl:"http://nus-iss-grp1-backend-user-alb-186458001.ap-southeast-1.elb.amazonaws.com/user",
+  userBaseUrl:"http://ec2-13-250-118-241.ap-southeast-1.compute.amazonaws.com/user",
   // product base url
-  productBaseUrl:"http://nus-iss-grp1-product-alb-1028734771.ap-southeast-1.elb.amazonaws.com/product",
+  productBaseUrl:"http//ec2-13-229-200-111.ap-southeast-1.compute.amazonaws.com/product",
   // order cart base url
-  orderBaseUrl:"http://nus-iss-grp1-backend-cart-alb-1700263701.ap-southeast-1.elb.amazonaws.com/cart"
+  orderBaseUrl:"http://ec2-18-140-61-88.ap-southeast-1.compute.amazonaws.com/cart"
 };
