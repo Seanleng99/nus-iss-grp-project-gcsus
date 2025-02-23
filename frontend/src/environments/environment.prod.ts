b50890bb646e1ed296fad725e1c69da61f3ff19d
Tan Sean Leng <seanleng99@gmail.com>
2025-02-23 20:53:50 +0800

    Update AWS endpoint

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,9 +1,9 @@
 export const environment = {
   production: true,
   // user base url
-  userBaseUrl:"http://ec2-13-229-197-130.ap-southeast-1.compute.amazonaws.com/user",
+  userBaseUrl:"http://ec2-3-1-20-232.ap-southeast-1.compute.amazonaws.com/user",
   // product base url
-  productBaseUrl:"http://ec2-54-255-184-203.ap-southeast-1.compute.amazonaws.com/product",
+  productBaseUrl:"http://ec2-18-142-104-142.ap-southeast-1.compute.amazonaws.com/product",
   // order cart base url
-  orderBaseUrl:"http://ec2-52-221-211-127.ap-southeast-1.compute.amazonaws.com/cart"
+  orderBaseUrl:"http://ec2-13-214-188-116.ap-southeast-1.compute.amazonaws.com/cart"
 };
