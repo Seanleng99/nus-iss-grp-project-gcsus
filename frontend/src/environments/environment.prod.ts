a36e3bbf585588cce9bb741d6729521345114c1b
Tan Sean Leng <seanleng99@gmail.com>
2025-02-23 16:16:52 +0800

    Update AWS endpoint

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,9 +1,9 @@
 export const environment = {
   production: true,
   // user base url
-  userBaseUrl:"http://nus-iss-gcsus-alb-user-957897179.ap-southeast-1.elb.amazonaws.com/user",
+  userBaseUrl:"http://ec2-47-129-174-177.ap-southeast-1.compute.amazonaws.com/user",
   // product base url
-  productBaseUrl:"http://nus-iss-gcsus-alb-product-672418563.ap-southeast-1.elb.amazonaws.com/product",
+  productBaseUrl:"http://ec2-54-255-184-203.ap-southeast-1.compute.amazonaws.com/product",
   // order cart base url
-  orderBaseUrl:"http://nus-iss-gcsus-alb-cart-1486107012.ap-southeast-1.elb.amazonaws.com/cart"
+  orderBaseUrl:"http://ec2-52-221-211-127.ap-southeast-1.compute.amazonaws.com/cart"
 };
